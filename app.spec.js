c4a0942a5d4e9394e70adf2b088b6a4c64f57d7e
nadaadel19 <101408403+nadaadel19@users.noreply.github.com>
2026-03-09 02:18:06 +0200

    Refactor app.spec.js with updated tests and mocks

--- app.spec.js
+++ app.spec.js
@@ -1,65 +1,138 @@
-const {sum, greeting, flag, animals, getobject, getOrders, applydiscount} = require('./app')
-const db = require('./db')
-// test('sum - should return 5 for 2+3', ()=>{
-//     // const result = sum(0.201,0.3)
-//     // expect(result).toBeCloseTo(0)
-// })
+const { sum, isEven, animals, isOrdaredById, getOrders, applyDiscount,fetchData } = require("./app")
+const { greeting } = require("./app")
+const db  = require('./db')
+const {default: axios } = require('axios');
+// made mock for the axios module kolo
+jest.mock('axios');
+/*
+mockReset
+.mock property
+mockImplementation
+mock modul
+*/
 
-// test('greeting - should hello ahmed', ()=>{
-//     // const result = sum(0.201,0.3)
-//     // expect(greeting('ahmed')).toMatch('hello ahmed ')
-// })
-// describe('flag', ()=>{
-//     it('should true for 4', ()=>{
-//         expect(flag(4)).toBeTruthy()
+// describe('isEven',()=>{
+//     it (' it should return true for 4', ()=>{
+//         expect(isEven(4)).toBeTruthy();
 //     })
-//     // it('should true for 5', ()=>{
-//     //     expect(flag(5)).toBeTruthy()
-//     // })
-//     it('should false for 5', ()=>{
-//         expect(flag(5)).toBeFalsy()
+//     it ('it should return false for 5', ()=>{
+//         expect(isEven(5)).toBeTruthy();
 //     })
-// })
-
-
-// test('animals array - should true for cate', ()=>{
-//     expect(animals).toContain('cate')
-// })
-
-// test('validation', ()=>{
-//     let x = 5
-//    // expect(x).not.toBeUndefined()
-//     expect(x).not.toBeNull()
-// })
-
-// describe("getobject", ()=>{
-//    it("should return order of id 1", ()=>{
-//         const res = getobject(1);
-//         //expect(res).toBe({id:1, price:10})
-//         //expect(res).toEqual({id:1, price:10})
-//         //expect(res).toMatchObject({id:1, price:10})
-//         expect(res).toHaveProperty("id",1)
-//    })
-//    it("should throw error if id is not defined", ()=>{
-//     //make function its value is the caller of the other function
-//     expect(()=>getobject()).toThrow() 
-
-// })
-// })
-
-// describe("getorders" , ()=>{
-//     it("should return some orders", async()=>{
-//         //const orders = await getOrders()
-//         //expect((await getOrders()).length).toBe(2)
-//         await expect(getOrders()).resolves.toContainEqual({id:1, price:20})
+//     it ('it should return false for 7', ()=>{
+//         expect(isEven(7)).toBeFalsy();
 //     })
 // })
-describe("applydiscount" , ()=>{
-    it("should apply discount 10% forn order price 10", ()=>{
-        db.getorder = function(orderid){
-            return {id:orderid, price:10}
-        }
-        const order = applydiscount(1)
-        expect(order).toEqual({id:1, price:9})
+describe('isOrdaredById',()=>{
+    it ('it should return order by id 1', ()=>{
+        const res = isOrdaredById(1);
+        //expect(res).toEqual({id:1, price:10}); //specific
+        expect(res).toMatchObject({id:1, price:10}); // not specific
+        expect(res).toHaveProperty('id', 1); 
+
     })
+    it ('it should throw Error if id is not defined', ()=>{
+        expect(()=>isOrdaredById()).toThrow(); // function call the error function
+        expect(()=>isOrdaredById()).toThrowError('id is not defined'); // function call the error function
+
+    })
+    
+    
+    
+})
+describe('getOrders',()=>{
+    it('should return some orders', async ()=>{
+        // const orders = await getOrders();
+        //expect(orders.length).toBe(2)
+        //expect ((await getOrders()).length ).toBe(2)
+       //await expect ( getOrders()).resolves.toContain({id:1, price:10})
+       //await expect ( getOrders()).resolves.toContainEqual({id:1, price:10})
+    })
+    
+})
+describe('applyDiscount',()=>{
+   it('should return dicount 10% for order price 10', ()=>{
+    ////mock////  problem it will change in the memory so when you call it again it will return the fake values
+
+    // const myFunc = jest.fn();
+    // myFunc.mockReturnValue(5);
+    // db.getOrders = function(orderid){
+    //     return {id:orderid, price:10};
+    // }
+    // const order = applyDiscount(1);
+    // expect(order).toEqual({id:1, price:9})
+    // console.log(myFunc());
+    /////same as the above but use reset
+    db.getOrders = jest.fn().mockReturnValue({id:1, price:10 })
+    db.updateOrder = jest.fn();
+    const order = applyDiscount(1);
+    expect(order).toEqual({id:1, price:9})
+    //db.getOrders.mockReset(); //=>
+    //console.log(db.getOrders.mock)
+    //expect(db.getOrders.mock.calls.length).toBe(1)
+    expect(db.getOrders.mock.calls[0][0]).toBe(1);
+    //expect(db.updateOrder.mock.calls.length).toBe(1);
+    //expect(db.updateOrder.mock.calls[0][0]).toEqual({id:1, price:9});
+    expect(db.updateOrder).toHaveBeenCalled();
+    expect(db.updateOrder).toHaveBeenCalledWith({id:1, price:9});
+    //////mockImplementation/////
+    db.getOrders = jest.fn().mockImplementation((id)=>{
+        if(id<5){
+            return {id, price:10}
+        }else{
+            return {id, price:8}
+        }
+    })
+   })
+   it('should return price 8', ()=>{
+        expect(applyDiscount(10)).toEqual({id:10, price:8})
+        expect(db.getOrders.mock.calls.length).toBe(1);
+   })
+      
+})
+describe('fetchData',()=>{
+    it('should return some data', async ()=>{
+    axios.get.mockResolvedValue({id:5})
+     const data = await fetchData();
+     expect(data).toEqual({id:5})
+     
+    })
+})
+
+// test ('sum - should return 2 + 3 = 5', ()=>{
+//     const result = sum(3,3);
+//     //expect(result).toBe(5);
+//     expect(result).toBeGreaterThanOrEqual(5);
+//     expect(sum(0.1, 0.3)).toBe(0.4);
+//     //expect(sum(0.102, 0.3)).toBe(0.4);//!!
+//     expect(sum(0.1, 0.3)).toBeCloseTo(0.4);
+
+// });
+test ('greeting - it should return hello ahmed', ()=>{
+    //expect(greeting('ahmed')).toBe('hello ahmed');
+    expect(greeting('ahmed')).toMatch('hello ahmed')
+})
+// test ('isEven - it should return true for 4', ()=>{
+//     expect(isEven(4)).toBeTruthy();
+// })
+// test ('isEven - it should return false for 5', ()=>{
+//     expect(isEven(5)).toBeTruthy();
+// })
+// test ('isEven - it should return false for 7', ()=>{
+//     expect(isEven(7)).toBeFalsy();
+// })
+
+// test('validation',()=>{
+//    let x = 10;
+//    let x = null; 
+//    let x = 2;
+
+//     expect(x).toBeDefined();
+//     expect(x).toBeUndefined();
+//     expect(x).toBeNull();
+//     expect(x).not.toBeNull();
+// })
+
+//////////array///////////
+test ('animals - it should return true for cat', ()=>{
+    expect(animals).toContain('cat');
 })
